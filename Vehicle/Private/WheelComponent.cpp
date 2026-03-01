d7c1bd295204f25e4389cd6851c5129b8b46a78d
JayDeffault <kiril-aka@mail.ru>
2026-03-02 04:23:48 +0600

    Refine wheel sweep setup to use explicit sweep mesh component

--- Vehicle/Private/WheelComponent.cpp
+++ Vehicle/Private/WheelComponent.cpp
@@ -1,6 +1,7 @@
 #include "WheelComponent.h"
 
 #include "DrawDebugHelpers.h"
+#include "Components/PrimitiveComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "Engine/World.h"
 
@@ -12,9 +13,155 @@ UWheelComponent::UWheelComponent()
 void UWheelComponent::BeginPlay()
 {
     Super::BeginPlay();
+
+    InitializeWheelComponents();
+}
+
+void UWheelComponent::InitializeWheelComponents()
+{
+    Body = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+
+    TArray<UActorComponent*> MeshComponents = GetOwner()->GetComponentsByClass(UStaticMeshComponent::StaticClass());
+
+    if (SweepCollisionComponent == nullptr)
+    {
+        for (UActorComponent* Component : MeshComponents)
+        {
+            UStaticMeshComponent* MeshComponent = Cast<UStaticMeshComponent>(Component);
+
+            if (MeshComponent != nullptr && MeshComponent->GetFName() == SweepCollisionComponentName)
+            {
+                SweepCollisionComponent = MeshComponent;
+                break;
+            }
+        }
+    }
+
+    if (VisualWheelMeshComponent == nullptr)
+    {
+        for (UActorComponent* Component : MeshComponents)
+        {
+            UStaticMeshComponent* MeshComponent = Cast<UStaticMeshComponent>(Component);
+
+            if (MeshComponent != nullptr && MeshComponent->GetFName() == VisualWheelComponentName)
+            {
+                VisualWheelMeshComponent = MeshComponent;
+                break;
+            }
+        }
+    }
+
+    if (VisualWheelMeshComponent != nullptr)
+    {
+        VisualMeshInitialRelativeLocation = VisualWheelMeshComponent->GetRelativeLocation();
+    }
+}
+
+bool UWheelComponent::PerformSuspensionSweep(FHitResult& OutBestHit, FVector& OutStart, FVector& OutEnd) const
+{
+    if (GetWorld() == nullptr || SweepCollisionComponent == nullptr)
+    {
+        return false;
+    }
+
+    const FVector SuspensionDirection = -GetUpVector();
+    OutStart = GetComponentLocation();
+    OutEnd = OutStart + SuspensionDirection * SuspensionLength;
+
+    FCollisionQueryParams QueryParams(SCENE_QUERY_STAT(WheelSuspensionSweep), false, GetOwner());
+    TArray<FHitResult> SweepHits;
+
+    const bool bHasAnyHit = GetWorld()->ComponentSweepMultiByChannel(
+        SweepHits,
+        SweepCollisionComponent,
+        OutStart,
+        OutEnd,
+        SweepCollisionComponent->GetComponentQuat(),
+        SweepChannel,
+        QueryParams,
+        FCollisionResponseParams::DefaultResponseParam);
+
+    if (!bHasAnyHit)
+    {
+        return false;
+    }
+
+    float ClosestDistanceSq = TNumericLimits<float>::Max();
+    bool bFoundBlockingHit = false;
+
+    for (const FHitResult& Hit : SweepHits)
+    {
+        if (!Hit.bBlockingHit || Hit.GetActor() == GetOwner())
+        {
+            continue;
+        }
+
+        const float CurrentDistanceSq = FVector::DistSquared(OutStart, Hit.ImpactPoint);
+
+        if (CurrentDistanceSq < ClosestDistanceSq)
+        {
+            ClosestDistanceSq = CurrentDistanceSq;
+            OutBestHit = Hit;
+            bFoundBlockingHit = true;
+        }
+    }
+
+    return bFoundBlockingHit;
 }
 
 void UWheelComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (Body == nullptr || SweepCollisionComponent == nullptr)
+    {
+        return;
+    }
+
+    FHitResult BestHit;
+    FVector SweepStart = FVector::ZeroVector;
+    FVector SweepEnd = FVector::ZeroVector;
+
+    const bool bHasGroundHit = PerformSuspensionSweep(BestHit, SweepStart, SweepEnd);
+    const FVector SuspensionAxis = GetUpVector();
+
+    if (bHasGroundHit)
+    {
+        const float HitDistance = FVector::Distance(SweepStart, BestHit.ImpactPoint);
+        const float CompressionDistance = FMath::Clamp(SuspensionLength - HitDistance, 0.0f, SuspensionLength);
+        const float CompressionRatio = CompressionDistance / SuspensionLength;
+
+        const float SpringForce = CompressionRatio * SpringStiffness;
+        const float VelocityAlongSuspension = FVector::DotProduct(Body->GetPhysicsLinearVelocityAtPoint(SweepStart), SuspensionAxis);
+        const float DampingForce = -VelocityAlongSuspension * DamperStiffness;
+        const float TotalSuspensionForce = FMath::Max(SpringForce + DampingForce, 0.0f);
+
+        Body->AddForceAtLocation(SuspensionAxis * TotalSuspensionForce, SweepStart);
+
+        if (VisualWheelMeshComponent != nullptr)
+        {
+            const float WheelOffset = -(HitDistance - WheelRadius);
+            VisualWheelMeshComponent->SetRelativeLocation(
+                VisualMeshInitialRelativeLocation + FVector(0.0f, 0.0f, WheelOffset));
+        }
+
+        if (bDrawDebug)
+        {
+            DrawDebugLine(GetWorld(), SweepStart, BestHit.ImpactPoint, FColor::Green, false, -1.0f, 0, 2.0f);
+            DrawDebugSphere(GetWorld(), BestHit.ImpactPoint, 8.0f, 12, FColor::Green, false, -1.0f, 0, 1.5f);
+        }
+    }
+    else
+    {
+        if (VisualWheelMeshComponent != nullptr)
+        {
+            VisualWheelMeshComponent->SetRelativeLocation(
+                VisualMeshInitialRelativeLocation + FVector(0.0f, 0.0f, -SuspensionLength));
+        }
+
+        if (bDrawDebug)
+        {
+            DrawDebugLine(GetWorld(), SweepStart, SweepEnd, FColor::Red, false, -1.0f, 0, 1.5f);
+        }
+    }
 }
--- Vehicle/Public/WheelComponent.h
+++ Vehicle/Public/WheelComponent.h
@@ -20,12 +20,42 @@ public:
     virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
-    UPROPERTY()
+    void InitializeWheelComponents();
+    bool PerformSuspensionSweep(FHitResult& OutBestHit, FVector& OutStart, FVector& OutEnd) const;
+
+private:
+    UPROPERTY(EditAnywhere, Category = "Suspension")
+    float SuspensionLength = 45.0f;
+
+    UPROPERTY(EditAnywhere, Category = "Suspension")
+    float SpringStiffness = 65000.0f;
+
+    UPROPERTY(EditAnywhere, Category = "Suspension")
+    float DamperStiffness = 4500.0f;
+
+    UPROPERTY(EditAnywhere, Category = "Suspension")
+    float WheelRadius = 18.0f;
+
+    UPROPERTY(EditAnywhere, Category = "Suspension")
+    TEnumAsByte<ECollisionChannel> SweepChannel = ECC_WorldStatic;
+
+    UPROPERTY(EditAnywhere, Category = "Setup")
+    FName SweepCollisionComponentName = TEXT("SweepCollisionComponent");
+
+    UPROPERTY(EditAnywhere, Category = "Setup")
+    FName VisualWheelComponentName = TEXT("VisualWheelMeshComponent");
+
+    UPROPERTY(EditAnywhere, Category = "Setup")
     UStaticMeshComponent* SweepCollisionComponent = nullptr;
 
-    UPROPERTY()
+    UPROPERTY(EditAnywhere, Category = "Setup")
     UStaticMeshComponent* VisualWheelMeshComponent = nullptr;
 
+    UPROPERTY(EditAnywhere, Category = "Debug")
+    bool bDrawDebug = false;
+
     UPROPERTY()
-    UStaticMeshComponent* Body = nullptr;
+    UPrimitiveComponent* Body = nullptr;
+
+    FVector VisualMeshInitialRelativeLocation = FVector::ZeroVector;
 };
